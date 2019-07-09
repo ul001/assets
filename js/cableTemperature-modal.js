179799ccecf38379e6ca4129a33c1ead6030e439
itisname <1346622652@qq.com>
2019-07-09 10:38:19 +0800

    实现线缆温度页面跳转，数据查询，图表切换功能

---
+++ js/cableTemperature-modal.js
@@ -0,0 +1,138 @@
+$(function () {
+    var info;
+    var params = {
+        fSubid:10100001,
+        pageNo:1,
+        pageSize:"",
+        startDate:"2019-07-09 00:00:00",
+        endDate:"2019-07-09 23:59:59"
+    };
+    var url = location.search;
+    if (url.indexOf("?") != -1) {
+        var string = url.substring(1);
+        var array = string.split("&");
+        $.each(array, function (index, val) {
+            var row = val.split("=");
+            params[row[0]] = row[1];
+        });
+    }
+
+    tool.getDataByAjax("http://116.236.149.162:8090/SubstationWEBV2/main/getTempABCResultHistoryList", params, function (data) {
+        info = data;
+        showChart(data.list);
+    });
+
+    function showChart(data) {
+        var time=[];
+        var tempA=[];
+        var tempB=[];
+        var tempC=[];
+        if(data.length>0){
+            $.each(data,function (index,val) {
+                time.push(val.f_CollectTime.substring(11,16));
+                tempA.push(val.f_TempA);
+                tempB.push(val.f_TempB);
+                tempC.push(val.f_TempC);
+            })
+        }
+        var option = {
+            tooltip : {
+                trigger : 'axis'
+            },
+            legend : {
+                data : ['温度A','温度B','温度C']
+            },
+            grid:{
+                left:'5%',
+                right:'3%',
+                top:'5%',
+                bottom:'8%'
+            },
+            toolbox : {
+                show : true,
+                feature : {
+                    saveAsImage:{
+                        type:'png',
+                        show:true,
+                        title:'保存为图片'
+                    },
+                    magicType : {
+                        show : true,
+                        type : [ 'line', 'bar' ]
+                    },
+                    restore : {
+                        show : true
+                    }
+                }
+            },
+            calculable : true,
+            xAxis : [ {
+                data :time
+            }],
+            yAxis : [ {
+                type:'value',
+                scale:true,
+            } ],
+            series : [ {
+                name : '温度A',
+                type : 'line',
+                data : tempA
+            }, {
+                name : '温度B',
+                type : 'line',
+                data : tempB
+            },{
+                name : '温度C',
+                type : 'line',
+                data : tempC
+            } ]
+        };
+        $("#contain").html("");
+        $("#contain").removeAttr('_echarts_instance_');
+        myChart = echarts.init($("#contain").get(0),'macarons');
+        myChart.setOption(option);
+    }
+
+    function showTable(data){
+        var tableData=[];
+        if(data.length>0){
+            $.each(data,function (index,val) {
+                var row ={};
+                row.time = val.f_CollectTime.substring(11,16);
+                row.tempA = val.f_TempA;
+                row.tempB = val.f_TempB;
+                row.tempC = val.f_TempC;
+                tableData.push(row);
+            })
+        }
+        var columns = [
+            [{field : 'time', title : '采集时间', valign:'middle', align : 'center', rowspan : 2},
+                {title : '温度(℃)', valign : "middle", align : 'center', colspan : 3}],
+            [{field : 'tempA', title : 'A', valign : "middle", align : 'center'},
+                {field : 'tempB', title : 'B', valign : "middle", align : 'center'},
+                {field : 'tempC', title : 'C', valign : "middle", align : 'center'}]
+        ];
+        $("#contain").html("");
+        $("#contain").html("<table id='table'></table>");
+        var height = $("#contain").height();
+        $("#table").bootstrapTable({
+            columns: columns,
+            data: tableData,
+            height:height
+        })
+    }
+
+    $("#showTable").click(function () {
+        showTable(info.list);
+    });
+
+    $("#showChart").click(function () {
+        showChart(info.list);
+    });
+
+    $("#return").click(function () {
+        location.href = "cableTemperature.html";
+    })
+
+
+});
--- js/cableTemperature.js
+++ js/cableTemperature.js
@@ -1,68 +1,61 @@
-$(function () {  
-    var url = "http://116.236.149.162:8090/SubstationWEB/main/getTempABCResult";
+$(function () {
+    var url = "http://116.236.149.162:8090/SubstationWEBV2/main/getTempABCResult";
     var params = {
         fSubid: "10100001",
         pageNo:1,
         pageSize:10
     };
-    getDataByAjax(url, params, function (data) {
-       creatList(data);
+    tool.getDataByAjax(url, params, function (data) {
+        creatList(data.list);
     });
 
-    function getDataByAjax(url, params, successCallback) {
-        $.ajax({
-            type: 'GET',
-            url: url,
-            data: params,
-            // beforeSend: function (request) {
-            //     request.setRequestHeader("Authorization", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjI0MzYxMDMsInVzZXJuYW1lIjoiYWRtaW4ifQ.iyJK8dR2Sw39-mWAZsLlfuY6cAtxjfPpZnsPrR9E0qQ")
-            // },
-            success: function (result) {
-                successCallback(result.data);
-            }
+    var f_MeterCode;
+
+    function creatList(data) {
+        $(".container").html('');
+        var string = '';
+        if (data.length > 0) {
+            $.each(data, function (key, val) {
+                var tempA = "--";
+                var tempB = "--";
+                var tempC = "--";
+                if (val.a != undefined) {
+                    tempA = val.a;
+                }
+                if (val.b != undefined) {
+                    tempB = val.b;
+                }
+                if (val.c != undefined) {
+                    tempC = val.c;
+                }
+
+                string = '<section>' +
+                    '<div class="tempDiv">' +
+                    '<h3>' + val.f_MeterName + '</h3>' +
+                    '<div class="tempNum">' +
+                    '<p>A:<span>' + tempA + '</span>℃</p>' +
+                    '<p>B:<span>' + tempB + '</span>℃</p>' +
+                    '<p>C:<span>' + tempC + '</span>℃</p>' +
+                    '</div>' +
+                    '<button class="search tempBtn" type="button" value="' + val.f_MeterCode + '">查 询</button>' +
+                    '</div>' +
+                    '</section>';
+                $(".container").append(string);
+            });
+
+            $(".tempBtn").click(function () {
+                var $this = $(this);
+                f_MeterCode = $this[0].value;
+            });
+        } else {
+            $(".container").append("暂无温度信息").css("text-align", "center");
+        }
+
+        $(".tempBtn").click(function () {
+            var F_MeterCode = $(this).attr("value");
+            location.href="cableTemperature-modal.html?F_MeterCode="+F_MeterCode;
         })
     }
 
-   var f_MeterCode;
-    function creatList(data) {
-        $("#tempContain").html('');
-        var string='';
-        if(data.length>0){
-            $.each(data,function (key,val) {
-                var tempA="--";
-                var tempB="--";
-                var tempC="--";
-                if(val.a!=undefined){
-                    tempA=val.a;
-                }
-                if(val.b!=undefined){
-                    tempB=val.b;
-                }
-                if(val.c!=undefined){
-                    tempC=val.c;
-                }
 
-                string='<div class="tempDiv temperature">' +
-                    '<h4>'+val.f_MeterName+'</h4>'+
-                    '<div class="tempPic">' +
-                    '<img src="app/image/temp.png"/>' +
-                    '</div>' +
-                    '<div class="tempNum">' +
-                    '<p>A:<span>'+tempA+'</span>℃</p>' +
-                    '<p>B:<span>'+tempB+'</span>℃</p>' +
-                    '<p>C:<span>'+tempC+'</span>℃</p>' +
-                    '</div>' +
-                    '<button class="search tempBtn" type="button" value="'+val.f_MeterCode+'">查 询</button>' +
-                    '</div>';
-                $("#tempContain").append(string);
-            });
-
-            $(".tempBtn").click(function(){
-                var $this = $(this);
-                f_MeterCode= $this[0].value;
-            });
-        }else {
-            $("#tempContain").append("暂无温度信息").css("text-align","center");
-        }
-    }
 });
