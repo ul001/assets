197d382c44350fc321e404e314e1716ab1687d81
xuhang <925645182@qq.com>
2019-07-16 13:57:30 +0800

    温湿度图表样式更改

--- js/TempHumidityData.js
+++ js/TempHumidityData.js
@@ -1,29 +1,29 @@
 $(function () {
-//     var baseUrlFromAPP="http://116.236.149.162:8090/SubstationWEBV2";
-//     var tokenFromAPP="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjM1MzczMTAsInVzZXJuYW1lIjoiYWRtaW4ifQ.ty4m082uqMhF_j846hQ-dVCiYOdepOWdDIr7UiV9eTI";
-//     var subidFromAPP=10100001;
+     var baseUrlFromAPP="http://116.236.149.162:8090/SubstationWEBV2";
+     var tokenFromAPP="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjM1MzczMTAsInVzZXJuYW1lIjoiYWRtaW4ifQ.ty4m082uqMhF_j846hQ-dVCiYOdepOWdDIr7UiV9eTI";
+     var subidFromAPP=10100001;
     //iOS安卓基础传参
-     var u = navigator.userAgent,
-         app = navigator.appVersion;
-     var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Linux') > -1; //安卓系统
-     var isIOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios系统
-     //判断数组中是否包含某字符串
-     var baseUrlFromAPP;
-     var tokenFromAPP;
-     var subidFromAPP;
-     if (isIOS) { //ios系统的处理
-         window.webkit.messageHandlers.iOS.postMessage(null);
-         var storage = localStorage.getItem("accessToken");
-         // storage = storage ? JSON.parse(storage):[];
-         storage = JSON.parse(storage);
-         baseUrlFromAPP = storage.baseurl;
-         tokenFromAPP = storage.token;
-         subidFromAPP = storage.fsubID;
-     } else {
-         baseUrlFromAPP = android.getBaseUrl();
-         tokenFromAPP = android.getToken();
-         subidFromAPP = android.getfSubid();
-     }
+//     var u = navigator.userAgent,
+//         app = navigator.appVersion;
+//     var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Linux') > -1; //安卓系统
+//     var isIOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios系统
+//     //判断数组中是否包含某字符串
+//     var baseUrlFromAPP;
+//     var tokenFromAPP;
+//     var subidFromAPP;
+//     if (isIOS) { //ios系统的处理
+//         window.webkit.messageHandlers.iOS.postMessage(null);
+//         var storage = localStorage.getItem("accessToken");
+//         // storage = storage ? JSON.parse(storage):[];
+//         storage = JSON.parse(storage);
+//         baseUrlFromAPP = storage.baseurl;
+//         tokenFromAPP = storage.token;
+//         subidFromAPP = storage.fsubID;
+//     } else {
+//         baseUrlFromAPP = android.getBaseUrl();
+//         tokenFromAPP = android.getToken();
+//         subidFromAPP = android.getfSubid();
+//     }
 
       function getData(url,params,successCallback) {
         $.ajax({
@@ -99,130 +99,87 @@ $(function () {
         });
       }
 
+      function initLineAnal(value,time,name,unit){
+        var option = {
+            tooltip:{
+                trigger:'axis'
+            },
+            toolbox:{
+                show:true,
+                orient:'horizontal',
+                top:-6,
+                feature:{
+                    dataView: {readOnly: true},
+                    dataZoom: {
+                        yAxisIndex: 'none'
+                    }
+                }
+            },
+            dataZoom: [{   // 这个dataZoom组件，默认控制x轴。
+            type: 'slider', // 这个 dataZoom 组件是 slider 型 dataZoom 组件
+            start: 0,      // 左边在 10% 的位置。
+            end: 100,         // 右边在 60% 的位置。
+            height:20,
+            bottom:6
+            }],
+            grid:{
+                left:'13%',
+                right:'11%',
+                top:'20%',
+                bottom:'24%'
+            },
+            xAxis:{
+                type:'category',
+                boundaryGap:false,
+                data:time
+            },
+            yAxis:{
+                type:'value',
+                scale:true,
+                axisLabel:{
+                    formatter:'{value}'+unit
+                }
+            },
+            series:[
+                {
+                    name:name,
+                    type:'line',
+                    data:value,
+                    markPoint:{
+                        symbol:'circle',
+                        symbolSize:10,
+                        data:[{name:'最大值',type:'max',
+                            label:{
+                                normal:{
+                                    formatter:'Max:{c}'
+                                }
+                            }},
+                            {name:'最小值',type:'min',
+                                label:{
+                                    normal:{
+                                        formatter:'Min:{c}'
+                                    }
+                                }}],
+                        itemStyle:{
+                            normal:{
+                                label:{
+                                    position:'top'
+                                }
+                            }
+                        }
+                    },
+                    markLine:{
+                        data:[{name:'平均值',type:'average'}]
+                    }
+                }
+            ]
+        };
+        return option;
+    }
+
       function setChart(chartData){
-        var option={
-            tooltip: {trigger: 'axis'},
-            title: {
-                text: '温度'
-            },
-            grid:{
-                left:'10%',
-                right:'3%',
-                top:'15%',
-                bottom:'20%'
-            },
-            toolbox : {
-                show : true,
-                feature : {
-                    saveAsImage:{
-                        type:'png',
-                        show:true,
-                        title:'保存为图片'
-                    },
-                    magicType : {
-                        show : true,
-                        type : [ 'line', 'bar' ]
-                    },
-                    restore : {
-                        show : true
-                    }
-                }
-            },
-            dataZoom: [{   // 这个dataZoom组件，默认控制x轴。
-            type: 'slider', // 这个 dataZoom 组件是 slider 型 dataZoom 组件
-            start: 0,      // 左边在 10% 的位置。
-            end: 100         // 右边在 60% 的位置。
-            }],
-            legend: {
-                data:['温度']
-            },
-            calculable: true,
-            xAxis: {
-                type: 'category',
-                data: chartData.times
-            },
-            yAxis: {
-                type:'value',
-                scale:true,
-            },
-            series: [{
-                name: '温度',
-                type: 'line',
-                data: chartData.temps,
-                markLine : {
-        　　　　　　 data : [
-        　　　　　　　　{type : 'average', name: '平均值'}
-        　　　　　　 ]
-                },
-                markPoint:{
-                    data:[
-                        {type : 'max', name: '最大值'},
-                        {type : 'min', name: '最小值'}
-                    ]
-                }
-            }]
-        };
-        var option2={
-            tooltip: {trigger: 'axis'},
-            title: {
-                text: '湿度'
-            },
-            grid:{
-                left:'10%',
-                right:'3%',
-                top:'15%',
-                bottom:'20%'
-            },
-            toolbox : {
-                show : true,
-                feature : {
-                    saveAsImage:{
-                        type:'png',
-                        show:true,
-                        title:'保存为图片'
-                    },
-                    magicType : {
-                        show : true,
-                        type : [ 'line', 'bar' ]
-                    },
-                    restore : {
-                        show : true
-                    }
-                }
-            },
-            calculable : true,
-            dataZoom: [{   // 这个dataZoom组件，默认控制x轴。
-            type: 'slider', // 这个 dataZoom 组件是 slider 型 dataZoom 组件
-            start: 0,      // 左边在 10% 的位置。
-            end: 100         // 右边在 60% 的位置。
-            }],
-            legend: {
-                data:['湿度']
-            },
-            xAxis: {
-                data: chartData.times
-            },
-            yAxis: {
-                type:'value',
-                scale:true,
-            },
-            series: [{
-                name: '湿度',
-                type: 'line',
-                data: chartData.humis,
-                markLine : {
-        　　　　　　 data : [
-        　　　　　　　　{type : 'average', name: '平均值'}
-        　　　　　　 ]
-                },
-                markPoint:{
-                    data:[
-                        {type : 'max', name: '最大值'},
-                        {type : 'min', name: '最小值'}
-                    ]
-                }
-            }]
-        };
+        var option=initLineAnal(chartData.temps,chartData.times,"温度","°C");
+        var option2=initLineAnal(chartData.humis,chartData.times,"湿度","%");
         var myChart = echarts.init($("#tempChart").get(0));
         myChart.setOption(option);
         var myChart2 = echarts.init($("#humiChart").get(0));
