e3e82aee66059fb6a5f5aee6b38d9418a9f3e757
sunnylam917 <372838945@qq.com>
2019-07-18 16:20:27 +0800

    遥测越限模态框样式修改

--- js/function.js
+++ js/function.js
@@ -1,35 +1,35 @@
 $(function () {
-    //    var baseUrlFromAPP="http://116.236.149.162:8090/SubstationWEBV2";
-    //    var tokenFromAPP="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjM1MzczMTAsInVzZXJuYW1lIjoiYWRtaW4ifQ.ty4m082uqMhF_j846hQ-dVCiYOdepOWdDIr7UiV9eTI";
-    //    var subidFromAPP=10100001;
+    var baseUrlFromAPP="http://116.236.149.162:8090/SubstationWEBV2";
+    var tokenFromAPP="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjM1MzczMTAsInVzZXJuYW1lIjoiYWRtaW4ifQ.ty4m082uqMhF_j846hQ-dVCiYOdepOWdDIr7UiV9eTI";
+    var subidFromAPP=10100001;
     //iOS安卓基础传参
-    var u = navigator.userAgent,
-        app = navigator.appVersion;
-    var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Linux') > -1; //安卓系统
-    var isIOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios系统
-    //判断数组中是否包含某字符串
-    var baseUrlFromAPP;
-    var tokenFromAPP;
-    var subidFromAPP;
-    if (isIOS) { //ios系统的处理
-        window.webkit.messageHandlers.iOS.postMessage(null);
-        var storage = localStorage.getItem("accessToken");
-        // storage = storage ? JSON.parse(storage):[];
-        storage = JSON.parse(storage);
-        baseUrlFromAPP = storage.baseurl;
-        tokenFromAPP = storage.token;
-        subidFromAPP = storage.fsubID;
-    } else {
-        baseUrlFromAPP = android.getBaseUrl();
-        tokenFromAPP = android.getToken();
-        subidFromAPP = android.getfSubid();
-        $("#meterName,#paramName").on("focus", function () {
-            var _this = this;
-            setTimeout(function () {
-                _this.scrollIntoViewIfNeeded();
-            }, 200);
-        });
-    }
+    // var u = navigator.userAgent,
+    //     app = navigator.appVersion;
+    // var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Linux') > -1; //安卓系统
+    // var isIOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios系统
+    // //判断数组中是否包含某字符串
+    // var baseUrlFromAPP;
+    // var tokenFromAPP;
+    // var subidFromAPP;
+    // if (isIOS) { //ios系统的处理
+    //     window.webkit.messageHandlers.iOS.postMessage(null);
+    //     var storage = localStorage.getItem("accessToken");
+    //     // storage = storage ? JSON.parse(storage):[];
+    //     storage = JSON.parse(storage);
+    //     baseUrlFromAPP = storage.baseurl;
+    //     tokenFromAPP = storage.token;
+    //     subidFromAPP = storage.fsubID;
+    // } else {
+    //     baseUrlFromAPP = android.getBaseUrl();
+    //     tokenFromAPP = android.getToken();
+    //     subidFromAPP = android.getfSubid();
+    //     $("#meterName,#paramName").on("focus", function () {
+    //         var _this = this;
+    //         setTimeout(function () {
+    //             _this.scrollIntoViewIfNeeded();
+    //         }, 200);
+    //     });
+    // }
 
     //创建MeScroll对象
     var mescroll = new MeScroll("mescroll", {
