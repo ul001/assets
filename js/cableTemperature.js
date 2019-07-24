8c8eeffb82dca638d6deff9b9de254a4519eec1c
sunnylam917 <372838945@qq.com>
2019-07-24 10:38:55 +0800

    遥测越限类型点选出现蓝框修改

--- js/cableTemperature.js
+++ js/cableTemperature.js
@@ -1,29 +1,29 @@
 $(function () {
-//    var baseUrlFromAPP = "http://116.236.149.162:8090/SubstationWEBV2";
-//    var tokenFromAPP = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjQwNjAwMDMsInVzZXJuYW1lIjoiYWRtaW4ifQ.cJLitLISm83qahqsXi9tMUrGbQ1aEiorhKe6k7RgJiE";
-//    var subidFromAPP = 10100001;
+    var baseUrlFromAPP = "http://116.236.149.162:8090/SubstationWEBV2";
+    var tokenFromAPP = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjQyMzE3NzMsInVzZXJuYW1lIjoiYWRtaW4ifQ.pfgcsrczhtQN9jwzgeM568npgMAUVsca-cd1AJoc6_s";
+   var subidFromAPP = 10100001;
     //iOS安卓基础传参
-       var u = navigator.userAgent,
-           app = navigator.appVersion;
-       var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Linux') > -1; //安卓系统
-       var isIOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios系统
-       //判断数组中是否包含某字符串
-       var baseUrlFromAPP;
-       var tokenFromAPP;
-       var subidFromAPP;
-       if (isIOS) { //ios系统的处理
-           window.webkit.messageHandlers.iOS.postMessage(null);
-           var storage = localStorage.getItem("accessToken");
-           // storage = storage ? JSON.parse(storage):[];
-           storage = JSON.parse(storage);
-           baseUrlFromAPP = storage.baseurl;
-           tokenFromAPP = storage.token;
-           subidFromAPP = storage.fsubID;
-       } else {
-           baseUrlFromAPP = android.getBaseUrl();
-           tokenFromAPP = android.getToken();
-           subidFromAPP = android.getfSubid();
-       }
+    //    var u = navigator.userAgent,
+    //        app = navigator.appVersion;
+    //    var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Linux') > -1; //安卓系统
+    //    var isIOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios系统
+    //    //判断数组中是否包含某字符串
+    //    var baseUrlFromAPP;
+    //    var tokenFromAPP;
+    //    var subidFromAPP;
+    //    if (isIOS) { //ios系统的处理
+    //        window.webkit.messageHandlers.iOS.postMessage(null);
+    //        var storage = localStorage.getItem("accessToken");
+    //        // storage = storage ? JSON.parse(storage):[];
+    //        storage = JSON.parse(storage);
+    //        baseUrlFromAPP = storage.baseurl;
+    //        tokenFromAPP = storage.token;
+    //        subidFromAPP = storage.fsubID;
+    //    } else {
+    //        baseUrlFromAPP = android.getBaseUrl();
+    //        tokenFromAPP = android.getToken();
+    //        subidFromAPP = android.getfSubid();
+    //    }
 
     //创建MeScroll对象
     var mescroll = new MeScroll("mescroll", {
