677e0cc1aa1942c3f04368a9d514aea674731f55
xuhang96 <45863623+xuhang96@users.noreply.github.com>
2019-11-28 10:33:22 +0800

    修复漏电检测无法点击

--- js/LeakageMonitor.js
+++ js/LeakageMonitor.js
@@ -1,7 +1,7 @@
 $(function () {
-//    var baseUrlFromAPP = "http://116.236.149.162:8090/SubstationWEBV2";
-//    var tokenFromAPP = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjQwNjAwMDMsInVzZXJuYW1lIjoiYWRtaW4ifQ.cJLitLISm83qahqsXi9tMUrGbQ1aEiorhKe6k7RgJiE";
-//    var subidFromAPP = 10100001;
+   // var baseUrlFromAPP = "http://122.192.163.218:5050/SubstationWEBV2/v3";
+   // var tokenFromAPP = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NzU1MTAwOTgsInVzZXJuYW1lIjoiYWRtaW4ifQ.BkWf-d8hBF5hov86iAjr147jGGXAvzS3un70T4D93PI";
+   // var subidFromAPP = 10100001;
     //iOS安卓基础传参
      var u = navigator.userAgent,
          app = navigator.appVersion;
@@ -27,56 +27,55 @@ $(function () {
 
     let toast = new ToastClass();//实例化toast对象
 
-    //创建MeScroll对象
-    var mescroll = new MeScroll("mescroll", {
-        down: {
-            auto: false, //是否在初始化完毕之后自动执行下拉回调callback; 默认true
-            callback: downCallback //下拉刷新的回调
-        },
-        up: {
-            auto: true, //是否在初始化时以上拉加载的方式自动加载第一页数据; 默认false
-            callback: upCallback, //上拉回调,此处可简写; 相当于 callback: function (page) { upCallback(page); }
-            empty: {
-                tip: "暂无相关数据", //提示
-            },
-            clearEmptyId: "container" //相当于同时设置了clearId和empty.warpId; 简化写法;默认null
-        }
-    });
+    // //创建MeScroll对象
+    // var mescroll = new MeScroll("mescroll", {
+    //     down: {
+    //         auto: false, //是否在初始化完毕之后自动执行下拉回调callback; 默认true
+    //         callback: downCallback //下拉刷新的回调
+    //     },
+    //     up: {
+    //         auto: true, //是否在初始化时以上拉加载的方式自动加载第一页数据; 默认false
+    //         callback: upCallback, //上拉回调,此处可简写; 相当于 callback: function (page) { upCallback(page); }
+    //         empty: {
+    //             tip: "暂无相关数据", //提示
+    //         },
+    //         clearEmptyId: "container" //相当于同时设置了clearId和empty.warpId; 简化写法;默认null
+    //     }
+    // });
 
-    /*下拉刷新的回调 */
-    function downCallback() {
-        mescroll.resetUpScroll();
-        //联网加载数据
-        getListDataFromNet(1, 20, function (data) {
-            //联网成功的回调,隐藏下拉刷新的状态
-            mescroll.endSuccess();
-            //设置列表数据
-            creatList(data.leakageCurrentValues);
-        }, function () {
-            //联网失败的回调,隐藏下拉刷新的状态
-            mescroll.endErr();
-        });
-    }
+    // /*下拉刷新的回调 */
+    // function downCallback() {
+    //     mescroll.resetUpScroll();
+    //     //联网加载数据
+    //     getListDataFromNet(1, 20, function (data) {
+    //         //联网成功的回调,隐藏下拉刷新的状态
+    //         mescroll.endSuccess();
+    //         //设置列表数据
+    //         creatList(data.leakageCurrentValues);
+    //     }, function () {
+    //         //联网失败的回调,隐藏下拉刷新的状态
+    //         mescroll.endErr();
+    //     });
+    // }
 
-    /*上拉加载的回调 page = {num:1, size:10}; num:当前页 从1开始, size:每页数据条数 */
-    function upCallback(page) {
-        getListDataFromNet(page.num, page.size, function (data) {
-            //联网成功的回调,隐藏下拉刷新和上拉加载的状态;
-            mescroll.endSuccess(data.leakageCurrentValues.length); //传参:数据的总数; mescroll会自动判断列表如果无任何数据,则提示空;列表无下一页数据,则提示无更多数据;
-            //设置列表数据
-            creatList(data.leakageCurrentValues);
-        }, function () {
-            //联网失败的回调,隐藏下拉刷新和上拉加载的状态;
-            mescroll.endErr();
-        });
-    }
-
-    function getListDataFromNet(num,page,successCallback,errorCallback){
+    // /*上拉加载的回调 page = {num:1, size:10}; num:当前页 从1开始, size:每页数据条数 */
+    // function upCallback(page) {
+    //     getListDataFromNet(page.num, page.size, function (data) {
+    //         //联网成功的回调,隐藏下拉刷新和上拉加载的状态;
+    //         mescroll.endSuccess(data.leakageCurrentValues.length); //传参:数据的总数; mescroll会自动判断列表如果无任何数据,则提示空;列表无下一页数据,则提示无更多数据;
+    //         //设置列表数据
+    //         creatList(data.leakageCurrentValues);
+    //     }, function () {
+    //         //联网失败的回调,隐藏下拉刷新和上拉加载的状态;
+    //         mescroll.endErr();
+    //     });
+    // }
+    getListDataFromNet();
+    
+    function getListDataFromNet(){
         var url = baseUrlFromAPP+"/energySecurity/leakageMonitor";
         var params = {
-            fSubid: subidFromAPP,
-            pageNo:num,
-            pageSize:page
+            fSubid: subidFromAPP
         };
         $.ajax({
             type: 'GET',
@@ -86,14 +85,13 @@ $(function () {
                 request.setRequestHeader("Authorization", tokenFromAPP)
             },
             success: function (result) {
-                successCallback && successCallback(result.data);
+                creatList(result.data.leakageCurrentValues);
             },
             error:function () {
-                errorCallback && errorCallback();
+                
             }
         })
     }
-    var f_MeterCode;
 
     function creatList(data) {
         $("#container").html('');
@@ -136,7 +134,7 @@ $(function () {
             });
         }
 
-        $(".tempBtn").click(function () {
+        $(".tempBtn").unbind().click(function () {
             var F_MeterCode = $(this).attr("value");
             var F_MeterName = $(this).attr("name");
             localStorage.setItem("fMeterName",F_MeterName);
